0c4e7afa59310e8892f6ff6dbce6e54a5f3546e1
Pravallika <96050882+prava-coder@users.noreply.github.com>
2022-03-13 14:59:11 +0530

    Add files via upload

---
+++ average_from_list.py
@@ -0,0 +1,10 @@
+# pyhton program to find the average from the list
+
+#creating a function
+def average_list():
+    print(lists)
+    return sum(lists)/len(lists)
+
+lists=[1,2,3,4]
+average=average_list()
+print("the total average from the list is:",round(average,1))
